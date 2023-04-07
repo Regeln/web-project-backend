da7db53f0d02bf5d50eee33f4948538e84660fe6
Regeln <regeln17@protonmail.com>
2023-04-07 23:09:59 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import path from "path";
 import items from "./items.js";
 
 const app = express();
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
 
 app.get("/api/products", (req, res) => {
     res.send(items);
@@ -11,4 +16,4 @@ const port = 4000;
 
 app.listen(port, () => {
     console.log(`Started server, port ${port}`);
-});Hello
+});
