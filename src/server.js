220f7206000b978dd10a3878b582c33b211194d7
Regeln <regeln17@protonmail.com>
2023-04-11 03:51:35 +0300

    Update server.js file

--- src/server.js
+++ src/server.js
@@ -1,14 +1,34 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
 import items from "./items.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use("/", require("./routes/root"));
+app.use(bodyParser.json());
 
-app.get("/api/products", (req, res) => {
+app.use(express.json());
+
+app.use(cookieParser());
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use("/", express.static(path.join(__dirname, "public")));
+
+// app.use("/", require("./routes/root"));
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+app.get("/products", (req, res) => {
     res.send(items);
 });
 
