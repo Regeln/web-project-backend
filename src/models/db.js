138397c6e395d6002719029e83d820869828a6c1
Regeln <regeln17@protonmail.com>
2023-04-11 15:46:39 +0300

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,13 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import pg from "pg";
 
-const pool = new pg.Pool(process.env.POSTGRES_URI);
+const pool = new pg.Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PWD,
+    port: process.env.POSTGRES_PORT,
+  });
 
 export { pool };
