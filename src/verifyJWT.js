d1c445b5c7fd5e4fd39be2c41be64c0bf2a8daab
Regeln <regeln17@protonmail.com>
2023-04-12 05:27:08 +0300

    Update verifyJWT function

--- src/verifyJWT.js
+++ src/verifyJWT.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers.Authorization;
+    const authHeader = req.headers.Authorization || req.headers.authorization;
     if (!authHeader?.startsWith("Bearer")) {
         return res.status(401).json({ message: "Unauthorized" });
     }
