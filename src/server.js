805cf78563bacfdf32180e700675c17e3a862e5e
Regeln <regeln17@protonmail.com>
2023-04-19 05:04:09 +0300

    Update server.js, add product route

--- src/server.js
+++ src/server.js
@@ -1,36 +1,23 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import bodyParser from "body-parser";
-import items from "./items.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 
-app.use(cors());
-
-app.use(bodyParser.json());
+app.use(cors({ credentials: true }));
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// app.use("/", express.static(path.join(__dirname, "public")));
-
-// app.use("/", require("./routes/root"));
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
-
-app.get("/products", (req, res) => {
-    res.send(items);
-});
+app.use("/product", productRoutes);
 
 const port = 4000;
 
